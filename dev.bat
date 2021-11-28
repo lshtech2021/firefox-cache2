3d2b8272d3c635b0ccef4dae4be33fab6e0df403
lshtech2021 <4025239+lshtech2021@users.noreply.github.com>
2021-11-28 16:06:29 +0800

    Update dev.bat
    Fix batch file.

--- dev.bat
+++ dev.bat
@@ -1,14 +1,5 @@
 @echo off
-@set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_301
-@set PATH=%JAVA_HOME%\bin;%PATH%
-@set PATH=E:\DevTools\apache-maven\bin;%PATH%
-@set PATH=E:\Programs\jboss-eap-7.3\bin;%PATH%
 
-@set PATH=C:\Home\flutter\bin;%PATH%
-@set PATH=C:\Home\flutter\bin;%PATH%
 @set PATH=C:\Programs\Python\Python39;C:\Programs\Python\Python39\Scripts;%PATH%
-@set PATH=C:\Programs\Redis;%PATH%
-@set PATH=C:\Home\DevTools\nodev14;%PATH%
-@set PATH=C:\Programs\aria2;%PATH%
 
 @cmd /k
